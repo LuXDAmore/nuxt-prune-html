ef042e253bb3e9fd6a6309f4b2318adb02e9e8dc
Athul Anil <athula@alokin.in>
2020-12-24 20:28:50 +0530

    fix: fix issues in setting headers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,42 @@
-export function cleanHeaders(
+function cleanHeaders(
  headers = {}
 ) {
 
+    if( headers && typeof headers === 'object' ) {
+
     for( const name in headers ) {
 
-        if( ! headers[ name ] )
-            delete this.headers[ name ];
+            if( name && ! headers[ name ] )
+                delete headers[ name ];
 
-    }
+        }
+
+}
     return headers;
+
+}
+
+export function setHeaders(
+ res, headers
+) {
+
+    const cleanedHeaders = cleanHeaders(
+ headers
+);
+
+    if( cleanedHeaders && Object.keys(
+ cleanedHeaders
+).length ) {
+
+for( const name in cleanedHeaders ) {
+
+res.setHeader(
+ name,
+headers[ name ]
+);
+
+}
+
+}
 
 }
